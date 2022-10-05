63128ff01e591bf82c1562b87fae572b36578747
slozche <ldhfj2141@gmail.com>
2022-10-05 15:23:31 +0900

    :sparkles:닉네임 소켓통신 프론트

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,24 +1,40 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`)
+
+const makeMessage = (type, payload) => {
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
     console.log("Connected to Server ✅")
 })
 
 socket.addEventListener("message", (message) => {
-    console.log("New message:", message.data)
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
 })
 
 socket.addEventListener("close", () => {
     console.log("Disconnected to Server ❎")
 })
 
-const handleSubmit = (event) => {
+const handleMessageSubmit = (event) => {
     event.preventDefault();
     const input = messageForm.querySelector("input");
-    socket.send(input.value);
+    socket.send(makeMessage("message", input.value));
     input.value = "";
 }
 
-messageForm.addEventListener("submit", handleSubmit)
+const handleNickSubmit = (event) => {
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+    input.value = "";
+}
+
+messageForm.addEventListener("submit", handleMessageSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
