887a88ab27278394e1d45ee0548430bf258f5713
slozche <ldhfj2141@gmail.com>
2022-10-06 17:11:10 +0900

    :sparkles: 룸 카운트 기능 구현

--- src/server.js
+++ src/server.js
@@ -15,22 +15,44 @@ const handleListen = () => console.log('Listening on http://localhost:3000');
 const httpServer = http.createServer(app);
 const io = SocketIO(httpServer);
 
+const publicRooms = () => {
+    const {sockets: {adapter: {sids, rooms}}} = io;
+    const publicRooms = [];
+    rooms.forEach((_, key) => {
+        if(sids.get(key) === undefined){
+            publicRooms.push(key);
+        };
+    });
+    return publicRooms;
+}
+
 io.on("connection", (socket) => {
     socket.onAny((event) => {
         console.log(`Socket Event:${event}`);
     });
+
     socket.on("nickname", nickname => (socket["nickname"] = nickname));
+
     socket.on("enter_room", (roomName, done) => {
         socket.join(roomName);
         done();
         socket.to(roomName).emit("welcome", socket.nickname);
+        io.sockets.emit("room_change", publicRooms());
     });
+
     socket.on("new_message", (msg, room, done) => {
         socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
         done();
-    })
+    });
+
     socket.on("disconnecting", () => {
-        socket.rooms.forEach(room => socket.to(room).emit("bye", socket.nickname));
+        socket.rooms.forEach(room => {
+            socket.to(room).emit("bye", socket.nickname)
+        })
+    });
+
+    socket.on("disconnect", () => {
+        io.sockets.emit("room_change", publicRooms());
     });
 })
 
