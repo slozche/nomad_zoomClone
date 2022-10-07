3c70efe527ab97c7dd24a2a8482f52e9fcd4e075
slozche <ldhfj2141@gmail.com>
2022-10-07 15:31:25 +0900

    ✨ 비디오,오디오 온오프 기능 추가 / 카메라 셀릭트 기능 UI

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,10 +3,26 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
 let myStream;
 let muted = false;
 let cameraOff = false;
+
+const getCameras = async () => {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      camerasSelect.append(option);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const getMedia = async () => {
   try {
@@ -15,6 +31,7 @@ const getMedia = async () => {
       video: true,
     });
     myFace.srcObject = myStream;
+    await getCameras();
   } catch (err) {
     console.log(err);
   }
@@ -23,6 +40,9 @@ const getMedia = async () => {
 getMedia();
 
 const handleMuteClick = () => {
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (!muted) {
     muteBtn.innerText = "Unmute";
     muted = true;
@@ -31,7 +51,11 @@ const handleMuteClick = () => {
     muted = false;
   }
 };
+
 const handleCameraClick = () => {
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (cameraOff) {
     cameraBtn.innerText = "Turn Camera Off";
     cameraOff = false;
@@ -41,5 +65,8 @@ const handleCameraClick = () => {
   }
 };
 
+const handleCameraChange = () => {};
+
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("change", handleCameraChange);
